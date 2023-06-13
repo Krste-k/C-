4bee492364853eeb8016f3bf9f9f80cfa279d702
Krste-k <krstevskikrste89@yahoo.com>
2023-06-13 18:56:44 +0200

    MVC

---
+++ MyFirstWepApp/MyFirstWepApp/Controllers/BookController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstWepApp.Models;
+
+namespace MyFirstWepApp.Controllers
+{
+
+    public class BookController : Controller
+    {
+
+        //https://localhost:7210/book/index
+        public IActionResult Index()
+        {
+            List<Book> books = StaticDb.Books;    
+            return View();
+        }
+
+        //https://localhost:7210/book/Empty
+        public IActionResult Empty ()
+        { 
+            return new EmptyResult();
+        }
+
+        //https://localhost:7210/book/GetJson
+        [Route("Json")]
+        public IActionResult GetJson() 
+        {
+            Book book = new Book()
+            {
+                Id = 1,
+                Title = "Kasni porasni"
+            };
+            return new JsonResult(book);
+        }
+
+        //https://localhost:7210/book/Redirect
+        public IActionResult Redirect ()
+        {
+            return RedirectToAction ("Index");
+        }
+
+        public IActionResult SecondRedirect()
+        {
+            return RedirectToAction("Privacy" , "Home");
+        }
+        public IActionResult Details (int? id)
+        {
+            if (id == null )
+            {
+                return new EmptyResult ();
+            }
+            return View();
+        }
+        Book book = StaticDb.Books.FirstOrDefault(book => book.Id == id);
+        private static int id;
+    }
+}
---
+++ MyFirstWepApp/MyFirstWepApp/StaticDb.cs
@@ -0,0 +1,26 @@
+﻿using MyFirstWepApp.Models;
+
+namespace MyFirstWepApp
+{
+    public class StaticDb
+    {
+        public static List<Book> Books = new List<Book>()
+        {
+           new Book
+           {
+               Id = 1,
+               Title = "Test",
+           },
+           new Book
+           {
+               Id = 2,
+               Title = "Test",
+           },
+           new Book
+           {
+               Id = 3,
+               Title = "Test",
+           }
+        };
+    }
+};
